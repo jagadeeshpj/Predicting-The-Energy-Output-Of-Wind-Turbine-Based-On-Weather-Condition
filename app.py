08cdc9d7f76858d65bd6fb90ddff60c6911e07b1
JAGADEESH P <jagadeeshpj2001@gmail.com>
2022-04-02 19:57:04 +0530

    wind

---
+++ app.py
@@ -0,0 +1,62 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import joblib
+import requests
+
+
+# NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
+API_KEY = "8r_73r_kobxkRh4xVZMTU29nikPAwCq3_2et93LJDKCj"
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey": API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+mltoken = token_response.json()["access_token"]
+
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+app = Flask(__name__)
+model = joblib.load('power_prediction.sav')
+@app.route('/')
+def home():
+    return render_template('intro.html')
+@app.route('/predict')
+def predict():
+    return render_template('predict.html')
+@app.route('/windapi',methods=['POST'])
+def windapi():
+    city=request.form.get('city')
+    apikey="86b1a085e43cad23bfd9c45d5fd88fc3"
+    url="http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apikey
+    resp = requests.get(url)
+    resp=resp.json()
+    temp = str((resp["main"]["temp"])-273.15) +" °C"
+    humid = str(resp["main"]["humidity"])+" %"
+    pressure = str(resp["main"]["pressure"])+" mmHG"
+    speed = str(resp["wind"]["speed"])+" m/s"
+    return render_template('predict.html', temp=temp, humid=humid, pressure=pressure,speed=speed)   
+@app.route('/y_predict',methods=['POST'])
+def y_predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    x_test = [[float(x) for x in request.form.values()]]
+    print(x_test)
+    payload_scoring = {"input_data": 
+			[{"field": [["Theoretical_Power_Curve (KWh)", "WindSpeed(m/s)"]], 
+			"values": x_test}]}
+
+    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/f33e7f01-076e-46b1-846a-7a3a74afa11b/predictions?version=2021-10-28', json=payload_scoring, headers={'Authorization': 'Bearer ' + mltoken})
+    print("Scoring response")
+    predictions =response_scoring.json()
+    print(predictions)
+    print('Final Prediction Result',predictions['predictions'][0]['values'][0][0])
+
+
+    pred =response_scoring.json()
+    print(pred)
+    #print('Final Prediction Result',predictions['predictions'][0]['values'][0][0])
+
+   # prediction = model.predict(x_test)
+    print(pred)
+    output = pred['predictions'][0]['values'][0][0]
+    return render_template('predict.html', prediction_text='The energy predicted is {:.2f} KWh'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
---
+++ windApp.py
@@ -0,0 +1,44 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import joblib
+import requests
+
+app = Flask(__name__)
+model = joblib.load('power_prediction.sav')
+
+@app.route('/')
+def home():
+    return render_template('intro.html')
+
+@app.route('/predict')
+def predict():
+    return render_template('predict.html')
+
+@app.route('/windapi',methods=['POST'])
+def windapi():
+    city=request.form.get('city')
+    apikey="86b1a085e43cad23bfd9c45d5fd88fc3"
+    url="http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apikey
+    resp = requests.get(url)
+    resp=resp.json()
+    temp = str((resp["main"]["temp"]) - 273.15) +" °C"
+    humid = str(resp["main"]["humidity"])+" %"
+    pressure = str(resp["main"]["pressure"])+" mmHG"
+    speed = str(resp["wind"]["speed"])+" m/s"
+    return render_template('predict.html', temp=temp, humid=humid, pressure=pressure,speed=speed)
+    
+@app.route('/y_predict',methods=['POST'])
+def y_predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    x_test = [[float(x) for x in request.form.values()]]
+    
+    prediction = model.predict(x_test)
+    print(prediction)
+    output=prediction[0]
+    return render_template('predict.html', prediction_text='The energy predicted is {:.2f} KWh'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
